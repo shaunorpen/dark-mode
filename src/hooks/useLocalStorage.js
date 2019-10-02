8dc326860b7ded7691fd444efea561352a6f4379
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-02 12:55:30 +0100

    Simplification to useLocalStorage()

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
-    const [storedValue, setStoredValue] = useState(() => {
-        return window.localStorage.getItem(key) ? JSON.parse(window.localStorage.getItem(key)) : initialValue;
-    });
+    const [storedValue, setStoredValue] = useState(JSON.parse(window.localStorage.getItem(key)) || initialValue);
 
     const setValue = (value) => {
         setStoredValue(value);
